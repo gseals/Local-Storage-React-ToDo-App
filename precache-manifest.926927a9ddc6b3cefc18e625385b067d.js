38d0444f5ff8764f32cff9e35535781423e97ab2
Gabriel Seals <sealsgj@gmail.com>
2020-07-07 11:27:36 -0500

    Updates

---
+++ precache-manifest.926927a9ddc6b3cefc18e625385b067d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba2e96f3efdbba208fa6e66547a7284",
+    "url": "/Local-Storage-React-ToDo-App/index.html"
+  },
+  {
+    "revision": "e46e107844ca5c5085c7",
+    "url": "/Local-Storage-React-ToDo-App/static/css/main.99c9bcf9.chunk.css"
+  },
+  {
+    "revision": "f4475b4646234bfc24e4",
+    "url": "/Local-Storage-React-ToDo-App/static/js/2.b96ae07b.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Local-Storage-React-ToDo-App/static/js/2.b96ae07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46e107844ca5c5085c7",
+    "url": "/Local-Storage-React-ToDo-App/static/js/main.66b5e702.chunk.js"
+  },
+  {
+    "revision": "ce71f9c6545ea12134ac",
+    "url": "/Local-Storage-React-ToDo-App/static/js/runtime-main.1364595d.js"
+  }
+]);
